fa403f42e0abaf98ad7d92f25bb611f213b8c629
Pham Quang <phamquang@Phams-MacBook-Pro.local>
2024-06-14 09:51:36 +0700

    feat: add pokecat project

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -13,72 +13,72 @@ import (
 )
 
 type Stats struct {
-	HP                 int `json:"HP"`
-	Attack 		   		int `json:"Attack"`                  
-	Defense            int `json:"Defense"`                 
-	Speed              int `json:"Speed"`                  
-	Sp_Attack          int `json:"Sp_Attack"`                  
-	Sp_Defense         int `json:"Sp_Defense"`                  
+	HP         int `json:"HP"`
+	Attack     int `json:"Attack"`
+	Defense    int `json:"Defense"`
+	Speed      int `json:"Speed"`
+	Sp_Attack  int `json:"Sp_Attack"`
+	Sp_Defense int `json:"Sp_Defense"`
 }
 
 type GenderRatio struct {
-	MaleRatio  float32 `json:"MaleRatio"` 
-	FemaleRatio float32 `json:"FemaleRatio"` 
+	MaleRatio   float32 `json:"MaleRatio"`
+	FemaleRatio float32 `json:"FemaleRatio"`
 }
 
 type Profile struct {
-	Height          float32 `json:"Height"`               
-	Weight          float32 `json:"Weight"`              
-	CatchRate 	 float32 `json:"CatchRate"`                  
-	GenderRatio     GenderRatio `json:"GenderRatio"` 
-	EggGroup        string `json:"EggGroup"` 
-	HatchSteps	  int `json:"HatchSteps"`
-	Abilities	   string `json:"Abilities"` 
+	Height      float32     `json:"Height"`
+	Weight      float32     `json:"Weight"`
+	CatchRate   float32     `json:"CatchRate"`
+	GenderRatio GenderRatio `json:"GenderRatio"`
+	EggGroup    string      `json:"EggGroup"`
+	HatchSteps  int         `json:"HatchSteps"`
+	Abilities   string      `json:"Abilities"`
 }
 
 type DamegeWhenAttacked struct {
-	Element     string `json:"Element"`  
-	Coefficient float32 `json:"Coefficient"` 
+	Element     string  `json:"Element"`
+	Coefficient float32 `json:"Coefficient"`
 }
 
 type Moves struct {
-	Name string `json:"Name"`
-	Element string `json:"Element"`
-	Power string `json:"Power"`
-	Acc 	int `json:"Acc"`
-	PP 		int `json:"PP"`
+	Name        string `json:"Name"`
+	Element     string `json:"Element"`
+	Power       string `json:"Power"`
+	Acc         int    `json:"Acc"`
+	PP          int    `json:"PP"`
 	Description string `json:"Description"`
 }
 
 type Pokemon struct {
-	Name 			string `json:"Name"`
-	Elements           []string `json:"Elements"`            
-	EV                 int `json:"EV"`
-	Stats              Stats `json:"Stats"`                                
-	Profile			Profile `json:"Profile"`              
+	Name               string               `json:"Name"`
+	Elements           []string             `json:"Elements"`
+	EV                 int                  `json:"EV"`
+	Stats              Stats                `json:"Stats"`
+	Profile            Profile              `json:"Profile"`
 	DamegeWhenAttacked []DamegeWhenAttacked `json:"DamegeWhenAttacked"`
-	EvolutionLevel    int `json:"EvolutionLevel"`
-	NextEvolution     string `json:"NextEvolution"`
-	Moves			  []Moves `json:"Moves"`                  
+	EvolutionLevel     int                  `json:"EvolutionLevel"`
+	NextEvolution      string               `json:"NextEvolution"`
+	Moves              []Moves              `json:"Moves"`
 }
 
 const (
 	numberOfPokemons = 649
-	baseURL = "https://pokedex.org/#/"
+	baseURL          = "https://pokedex.org/#/"
 )
 
-var pokemons []Pokemon;
+var pokemons []Pokemon
 
-func main(){
+func main() {
 	crawlPokemonsDriver(numberOfPokemons)
 }
 
-func crawlPokemonsDriver(numsOfPokemons int){
+func crawlPokemonsDriver(numsOfPokemons int) {
 	pw, err := playwright.Run()
 	if err != nil {
 		log.Fatalf("could not start playwright: %v", err)
 	}
-	browser, err := pw.Chromium.Launch();
+	browser, err := pw.Chromium.Launch()
 	if err != nil {
 		log.Fatalf("could not launch browser: %v", err)
 	}
@@ -89,15 +89,15 @@ func crawlPokemonsDriver(numsOfPokemons int){
 	}
 
 	page.Goto(baseURL)
-	
-	for i := range numsOfPokemons{
+
+	for i := range numsOfPokemons {
 		// simulate clicking the button to open the pokemon details
-		locator := fmt.Sprintf("button.sprite-%d", i + 1)
+		locator := fmt.Sprintf("button.sprite-%d", i+1)
 		button := page.Locator(locator).First()
 		time.Sleep(500 * time.Millisecond)
 		button.Click()
 
-		fmt.Print("Pokemon ", i + 1, " ")
+		fmt.Print("Pokemon ", i+1, " ")
 		crawlPokemons(page)
 
 		page.Goto(baseURL)
@@ -109,7 +109,7 @@ func crawlPokemonsDriver(numsOfPokemons int){
 	if err != nil {
 		log.Fatal(err)
 	}
-	os.WriteFile("pokedex.json", js, 0644)
+	os.WriteFile("../lib/pokedex.json", js, 0644)
 
 	if err = browser.Close(); err != nil {
 		log.Fatalf("could not close browser: %v", err)
@@ -119,7 +119,7 @@ func crawlPokemonsDriver(numsOfPokemons int){
 	}
 }
 
-func crawlPokemons(page playwright.Page){
+func crawlPokemons(page playwright.Page) {
 	pokemon := Pokemon{}
 
 	stats := Stats{}
@@ -127,26 +127,26 @@ func crawlPokemons(page playwright.Page){
 	for _, entry := range entries {
 		title, _ := entry.Locator("span:not([class])").TextContent()
 		switch title {
-			case "HP":
-				hp, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
-				stats.HP, _ = strconv.Atoi(hp)
-			case "Attack":
-				attack, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
-				stats.Attack, _ = strconv.Atoi(attack)
-			case "Defense":
-				defense, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
-				stats.Defense, _ = strconv.Atoi(defense)
-			case "Speed":
-				speed, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
-				stats.Speed, _ = strconv.Atoi(speed)
-			case "Sp Atk":
-				sp_Attack, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
-				stats.Sp_Attack, _ = strconv.Atoi(sp_Attack)
-			case "Sp Def":
-				sp_Defense, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
-				stats.Sp_Defense, _ = strconv.Atoi(sp_Defense)
-			default:
-				fmt.Println("Unknown title: ", title)
+		case "HP":
+			hp, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
+			stats.HP, _ = strconv.Atoi(hp)
+		case "Attack":
+			attack, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
+			stats.Attack, _ = strconv.Atoi(attack)
+		case "Defense":
+			defense, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
+			stats.Defense, _ = strconv.Atoi(defense)
+		case "Speed":
+			speed, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
+			stats.Speed, _ = strconv.Atoi(speed)
+		case "Sp Atk":
+			sp_Attack, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
+			stats.Sp_Attack, _ = strconv.Atoi(sp_Attack)
+		case "Sp Def":
+			sp_Defense, _ := entry.Locator("span.stat-bar > div.stat-bar-fg").TextContent()
+			stats.Sp_Defense, _ = strconv.Atoi(sp_Defense)
+		default:
+			fmt.Println("Unknown title: ", title)
 		}
 	}
 	pokemon.Stats = stats
@@ -161,46 +161,46 @@ func crawlPokemons(page playwright.Page){
 		title1, _ := entry.Locator("strong:not([class]):nth-child(1)").TextContent()
 		stat1, _ := entry.Locator("span:not([class]):nth-child(2)").TextContent()
 		switch title1 {
-			case "Height:":
-				heights := strings.Split(stat1, " ")
-				height, _ := strconv.ParseFloat(heights[0], 32)
-				profile.Height = float32(height)
-			case "Catch Rate:":
-				catchRates := strings.Split(stat1, "%")
-				catchRate, _ := strconv.ParseFloat(catchRates[0], 32)
-				profile.CatchRate = float32(catchRate)
-			case "Egg Groups:":
-				profile.EggGroup = stat1
-			case "Abilities:":
-				profile.Abilities = stat1
+		case "Height:":
+			heights := strings.Split(stat1, " ")
+			height, _ := strconv.ParseFloat(heights[0], 32)
+			profile.Height = float32(height)
+		case "Catch Rate:":
+			catchRates := strings.Split(stat1, "%")
+			catchRate, _ := strconv.ParseFloat(catchRates[0], 32)
+			profile.CatchRate = float32(catchRate)
+		case "Egg Groups:":
+			profile.EggGroup = stat1
+		case "Abilities:":
+			profile.Abilities = stat1
 		}
 
 		title2, _ := entry.Locator("strong:not([class]):nth-child(3)").TextContent()
 		stat2, _ := entry.Locator("span:not([class]):nth-child(4)").TextContent()
 		switch title2 {
-			case "Weight:":
-				weights := strings.Split(stat2, " ")
-				weight, _ := strconv.ParseFloat(weights[0], 32)
-				profile.Weight = float32(weight)
-			case "Gender Ratio:":
-				if stat2 == "N/A" {
-					genderRatio.MaleRatio = 0
-					genderRatio.FemaleRatio = 0
-				} else {
-					ratios := strings.Split(stat2, " ")
+		case "Weight:":
+			weights := strings.Split(stat2, " ")
+			weight, _ := strconv.ParseFloat(weights[0], 32)
+			profile.Weight = float32(weight)
+		case "Gender Ratio:":
+			if stat2 == "N/A" {
+				genderRatio.MaleRatio = 0
+				genderRatio.FemaleRatio = 0
+			} else {
+				ratios := strings.Split(stat2, " ")
 
-					maleRatios := strings.Split(ratios[0], "%")
-					maleRatio, _ := strconv.ParseFloat(maleRatios[0], 32)
-					genderRatio.MaleRatio = float32(maleRatio)
+				maleRatios := strings.Split(ratios[0], "%")
+				maleRatio, _ := strconv.ParseFloat(maleRatios[0], 32)
+				genderRatio.MaleRatio = float32(maleRatio)
 
-					femaleRatios := strings.Split(ratios[2], "%")
-					femaleRatio, _ := strconv.ParseFloat(femaleRatios[0], 32)
-					genderRatio.FemaleRatio = float32(femaleRatio)
-				}
-				
-				profile.GenderRatio = genderRatio
-			case "Hatch Steps:":
-				profile.HatchSteps, _ = strconv.Atoi(stat2)
+				femaleRatios := strings.Split(ratios[2], "%")
+				femaleRatio, _ := strconv.ParseFloat(femaleRatios[0], 32)
+				genderRatio.FemaleRatio = float32(femaleRatio)
+			}
+
+			profile.GenderRatio = genderRatio
+		case "Hatch Steps:":
+			profile.HatchSteps, _ = strconv.Atoi(stat2)
 		}
 	}
 	pokemon.Profile = profile
@@ -228,8 +228,8 @@ func crawlPokemons(page playwright.Page){
 		evolutionLabel, _ := entry.Locator("div.evolution-label > span").TextContent()
 		evolutionLabels := strings.Split(evolutionLabel, " ")
 
-		if(evolutionLabels[0] == name){
-			evolutionLevels := strings.Split(evolutionLabels[len(evolutionLabels) - 1], ".")
+		if evolutionLabels[0] == name {
+			evolutionLevels := strings.Split(evolutionLabels[len(evolutionLabels)-1], ".")
 			evolutionLevel, _ := strconv.Atoi(evolutionLevels[0])
 			pokemon.EvolutionLevel = evolutionLevel
 
@@ -272,7 +272,7 @@ func crawlPokemons(page playwright.Page){
 		pokemon.Elements = append(pokemon.Elements, element)
 	}
 
-	fmt.Println(name, ": ",profile)
+	fmt.Println(name, ": ", profile)
 
 	pokemons = append(pokemons, pokemon)
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,6 @@ module PokemonBattle
 
 go 1.22.1
 
-require (
-	github.com/PuerkitoBio/goquery v1.9.2
-	golang.org/x/net v0.26.0 // indirect
-)
-
-require github.com/andybalholm/cascadia v1.3.2 // indirect
-
 require github.com/playwright-community/playwright-go v0.4401.0
 
 require (
